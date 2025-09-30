e7b56fc69387f10ce30b623a3ac93633603b770c
adamasimak13 <adamasimak13@gmail.com>
2025-09-30 04:58:06 +0300

    Update common.h

--- matrix_multiplication/common.h
+++ matrix_multiplication/common.h
@@ -1,13 +1,3 @@
-//===- matrix_multiplication.h ----------------------------000---*- C++ -*-===//
-//
-// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-// Copyright (C) 2024, Advanced Micro Devices, Inc.
-//
-//===----------------------------------------------------------------------===//
-
 // This file contains common helper functions for the matrix multiplication
 // host code, such as verifying and printing matrices.
 
